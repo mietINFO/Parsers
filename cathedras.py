8a33247c19722ab59dc5703b31a4420225dd3b7d
golichnikov <golichnikov.k.o@gmail.com>
2018-04-13 20:10:10 +0300

    Cathedra parser(Seems like it works correctly)

---
+++ cathedras.py
@@ -0,0 +1,88 @@
+import sqlite3
+import re
+import urllib.request
+from bs4 import BeautifulSoup
+
+LINK_URL = 'https://miet.ru/structure/e/3110'
+MIET_LINK = 'https://miet.ru'
+
+def get_html(url):
+    response = urllib.request.urlopen(url)
+    return response.read()
+
+
+def get_links(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    cathedras = soup.find('div', class_ = 'news-detail')
+    links = cathedras.find('ul')
+
+    cathedraLinks = []
+
+    for link in links.find_all('li'):
+        cathedraLinks.append({
+            'cathedra': link.find('a').text,
+            'link': MIET_LINK + link.find('a').get('href')
+        })
+    return cathedraLinks
+
+
+def parse(html, cathedraName, link):
+    soup = BeautifulSoup(html, 'html.parser')
+    information = soup.find('div', {'id': 'eli_detail'})
+
+    cathedraInfo = []
+
+    mailPattern = r'\w+@\w+.\w{1,4}'
+    hallPattern = r':\s*(\d{4}\w*)'
+    phonePattern = r'(\W\d{3}\W\s\d{3}-\d{2}-\d{2})\s*'
+
+    phone = re.search(phonePattern, str(information))
+    mail = re.search(mailPattern, str(information))
+    hall = re.search(hallPattern, str(information))
+
+    cathedraInfo.append({
+        'cathedra': cathedraName,
+        'head': information.find_all('b')[0].text + ': ' + information.find('a').text,
+        'phone': phone.group(0) if phone else 'не указан',
+        'mail': mail.group(0) if mail else 'не указан',
+        'hall': hall.group(0)[2:] if hall else 'не указана',
+        'link': link
+    })
+    return cathedraInfo
+
+
+def save(cathedras, db_file):
+    db = sqlite3.connect(db_file)
+    cursor = db.cursor()
+
+    # Создаём таблицу users, если она не существует
+    cursor.execute("""CREATE TABLE IF NOT EXISTS cathedras
+                                      (
+                                        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                                        cathedra TEXT,
+                                        head TEXT,
+                                        phone TEXT,
+                                        hall TEXT,
+                                        mail TEXT,
+                                        link TEXT
+                                      )""")
+
+    for cathedra in cathedras:
+        cursor.execute(
+            'INSERT INTO cathedras (cathedra, head, phone, hall, mail, link) VALUES (?, ?, ?, ?, ?, ?)',
+            (cathedra['cathedra'], cathedra['head'], cathedra['phone'], cathedra['hall'], cathedra['mail'],
+             cathedra['link']))
+
+
+def main():
+    links = get_links(get_html(LINK_URL))
+    parser = []
+
+    for link in links:
+        parser.extend(parse(get_html(link['link']), link['cathedra'], link['link']))
+
+    save(parser, 'cathedras.db')
+
+
+if __name__ == '__main__':
+    main()
